32275a1b818049383419c6cb4a5960225e54a1a7
Walter Fan (yafan) <yafan@cisco.com>
2019-12-07 21:38:57 +0800

    update example

--- potato/setenv.sh
+++ potato/setenv.sh
@@ -1,5 +1,6 @@
+export MYSQL_USER=walter
+export MYSQL_PWD=pass1234
 export EMAIL_SMTP_SERVER=smtp.163.com
-export EMAIL_USER=fanxyz@163.com
-export EMAIL_PWD=fanxyz
-
-
+export EMAIL_SMTP_PORT=25
+export EMAIL_USER=changeme@163.com
+export EMAIL_PWD=changeme
