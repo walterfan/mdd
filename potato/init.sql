211d066278ab4483ed1cbdce2e03c807b11b9301
Walter Fan (yafan) <yafan@cisco.com>
2019-12-30 20:58:41 +0800

    add examples

--- potato/init.sql
+++ potato/init.sql
@@ -1,6 +1,6 @@
 create database if not exists potato;
 create database if not exists scheduler;
 create database if not exists userservice;
-grant all on potato.* to 'walter'@'%';
-grant all on scheduler.* to 'walter'@'%';
-grant all on userservice.* to 'walter'@'%';
+#grant all on potato.* to 'walter'@'%';
+#grant all on scheduler.* to 'walter'@'%';
+#grant all on userservice.* to 'walter'@'%';
