8a5966240705aaabf4bbc6e6f551163578ef42e3
Walter Fan <walter.fan@gmail.com>
2020-07-03 03:20:23 +0000

    add a sample setenv.sh

---
+++ potato/setenv.sh
@@ -0,0 +1,6 @@
+export EMAIL_SMTP_SERVER=smtp.gmail.com
+export EMAIL_USER=potato@gmail.com
+export EMAIL_PWD=pass1234
+export MYSQL_PWD=pass1234
+export MYSQL_URL="jdbc:mysql://mysqldb/scheduler?useUnicode=true&characterEncoding=utf8"
+export MYSQL_USER=potato
